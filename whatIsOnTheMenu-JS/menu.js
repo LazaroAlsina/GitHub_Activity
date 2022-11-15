7b648ddf072458ad046c2c529a66bdf35f7b29d9
Lazaro <lazaroalsina@gmail.com>
2022-11-15 12:51:07 -0500

    We had created vars for our menu program.

---
+++ whatIsOnTheMenu-JS/menu.js
@@ -0,0 +1,10 @@
+/*Lets make a menu for Jorge!
+
+Please make a menu for food, you can use a list of "if" or switch statments to make a simple menu.
+Make sure to use varabiles to save the options that a user selected and display them on the console.
+Good luck!
+*/
+
+let menu=["fries", "bfWing", "chesse","pie", "onion rings", "chesse dip", "chesse sink", "salsa dip", "stake steak", "chesse pund sink"]
+let ui1="Please pick"
+let ui2="Please pick"
