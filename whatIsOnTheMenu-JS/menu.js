fc1ec0c209941dd8b4c51f415fe323bca19b668c
Lazaro <lazaroalsina@gmail.com>
2022-11-15 13:19:42 -0500

    finished our menu but its really bad... for now!

--- whatIsOnTheMenu-JS/menu.js
+++ whatIsOnTheMenu-JS/menu.js
@@ -11,11 +11,89 @@ let ui2="Please pick"
 
 //Manually adding what the user want. IT would be nice if someone can program a automatic way of placing the input here.
 //We need a loop that can find the numbers and match them base on the arry to the menu in the future.
-ui1="fries";
+ui1="salsa dip";
+ui2="chese hiuofauhjfasuhfaouhoufa "
 
-
+// I really wish we had loops! and Functions!
+//I really want a function for my program!
 if(ui1 === menu[0]) {
-    ui1 = menu[0]
-}
+    ui1 = menu[0];
+} 
+    else if (ui1 === menu[1]) {
+        ui1 = menu[1]; 
+    }
+    else if (ui1 === menu[2]) {
+        ui1 = menu[2]; 
+    }
+    else if (ui1 === menu[3]) {
+        ui1 = menu[3]; 
+    }
+    else if (ui1 === menu[4]) {
+        ui1 = menu[4]; 
+    }
+    else if (ui1 === menu[5]) {
+        ui1 = menu[5]; 
+    }
+    else if (ui1 === menu[6]) {
+        ui1 = menu[6]; 
+    }
+    else if (ui1 === menu[7]) {
+        ui1 = menu[7]; 
+    }
+    else if (ui1 === menu[8]) {
+        ui1 = menu[8]; 
+    }
+    else if (ui1 === menu[9]) {
+        ui1 = menu[9];
+    }
+    else if (ui1 === menu[10]) {
+        ui1 = menu[10]; 
+    }
+    else if (ui1 === menu[11]) {
+        ui1 = menu[11];  
+    } else {
+        ui1="error"
+    }
 
-console.log(ui1);
+    if(ui2 === menu[0]) {
+        ui2 = menu[0];
+    } 
+        else if (ui2 === menu[1]) {
+            ui2 = menu[1]; 
+        }
+        else if (ui2 === menu[2]) {
+            ui2 = menu[2]; 
+        }
+        else if (ui2 === menu[3]) {
+            ui2 = menu[3]; 
+        }
+        else if (ui2 === menu[4]) {
+            ui2 = menu[4]; 
+        }
+        else if (ui2 === menu[5]) {
+            ui2 = menu[5]; 
+        }
+        else if (ui2 === menu[6]) {
+            ui2 = menu[6]; 
+        }
+        else if (ui2 === menu[7]) {
+            ui2 = menu[7];
+            console.log("I stop here") 
+        }
+        else if (ui1 === menu[8]) {
+            ui1 = menu[8]; 
+        }
+        else if (ui1 === menu[9]) {
+            ui1 = menu[9];
+        }
+        else if (ui1 === menu[10]) {
+            ui1 = menu[10]; 
+        }
+        else if (ui1 === menu[11]) {
+            ui1 = menu[11];  
+        } else {
+            ui2="error"
+            console.log("This should come out")
+        }
+
+console.log(ui1,ui2);
