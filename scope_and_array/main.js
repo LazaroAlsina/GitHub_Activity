8525d202006d4732fa5d4454618cbc48e5f50f2e
Lazaro <lazaroalsina@gmail.com>
2022-11-17 17:57:28 -0500

    Teaching scope and arrays

---
+++ scope_and_array/main.js
@@ -0,0 +1,64 @@
+//Talk about scope here:
+
+//What is scope?
+
+
+//Block scope
+
+
+//Global Scope
+
+
+//Block scope
+
+
+//Scope Pollution
+
+
+//Best pratice with scoping
+
+
+//Reviewing scope
+
+
+
+//Talk about array here
+
+
+//What are arrays?
+
+
+//How to create an array
+
+
+//what is inside an array? What is it and what can it hold?
+
+
+//How to update your elements within an array
+
+
+//Arrays with let and const. Why does this happen?
+
+
+//How to use property(methods) with arrays.
+//.length , .push(), .pop()
+
+
+//How to use arrays and functions together.
+/*
+Example from codecademy
+const flowers = ['peony', 'daffodil', 'marigold'];
+ 
+function addFlower(arr) {
+  arr.push('lily');
+}
+ 
+addFlower(flowers);
+ 
+console.log(flowers); // Output: ['peony', 'daffodil', 'marigold', 'lily']
+
+*/
+
+
+//How to nest arrays and access them too.
+
